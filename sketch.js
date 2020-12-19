4529bb728b703fb3fec6c892c3e168972d6a15eb
RaaghavAnand <74807940+RaaghavAnand@users.noreply.github.com>
2020-12-19 12:20:58 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -23,24 +23,30 @@ function setup() {
 function draw() {
   background(0,0,0);  
 
-  if(wall.x-bullet.x < (bullet.width+wall.width/2)) {
+  if(hasCollided(bullet,wall)) { 
+ 
 
-   bullet.velocityX=0;
-   var deformation=0.5 * weight * speed * speed/22509;
-   if(deformation>180){
-     bullet.shapeColor=color(255,0,0);
-   }
+    bullet.velocityX=0; 
+ 
+    var damage= 0.5 *weight * speed* speed/(thickness *thickness *thickness); 
 
-  if(deformation<180 && deformation>100){
-    bullet.shapeColor=color(230,230,0);
-  }
+ 
+   if(damage>10){ 
+ 
+     wall.shapeColor=color(255,0,0); 
+ 
+   }  
+ 
+   if(damage<10){ 
+ 
+     wall.shapeColor=color(0,255,0); 
+ 
+   } 
   
-  if(deformation<100){
-    bullet.shapeColor=color(0,255,0);
-  }
 
-  }
-
+ 
+   } 
+ 
   
 
   drawSprites();
@@ -48,7 +54,7 @@ function draw() {
 
 function hasCollided(lbullet,lwall){
 
-  bulletRightEdge=lbullet.x +lbulltet.width,
+  bulletRightEdge=lbullet.x +lbullet.width;
   wallLeftEdge=lwall.x;
   if(bulletRightEdge>=wallLeftEdge){
     return true;
